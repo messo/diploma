67a4c966ad3c8dfddb2e5d277cf946bc5936c239
Bálint Kriván <balint@krivan.hu>
2015-04-20 13:06:37 +0200

    archiving old report + adding new structure

---
+++ src/final/FPSCounter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by balint on 2015.04.19..
+//
+
+#include "FPSCounter.h"
---
+++ src/final/FPSCounter.h
@@ -0,0 +1,13 @@
+//
+// Created by balint on 2015.04.19..
+//
+
+#pragma once
+
+
+class FPSCounter {
+
+};
+
+
+
